f1aa8b50fba5b3325b9422a512fd253f450c11df
Priyadharshini <dharshinihelianthus21@gmail.com>
2024-01-10 16:41:54 -0800

    anothre comment

--- src/main/java/com/example/demo/springdatajpa/Application.java
+++ src/main/java/com/example/demo/springdatajpa/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		//Hello everyone
 		SpringApplication.run(Application.class, args);
 	}
 
